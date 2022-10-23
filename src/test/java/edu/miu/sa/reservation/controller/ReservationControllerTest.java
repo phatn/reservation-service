81a3f097dffd220b3949b5b29c320b627c06c42e
Phat Nguyen <phatnguyentanit@gmail.com>
2022-10-22 19:59:12 -0500

    Added test case

--- src/test/java/edu/miu/sa/reservation/controller/ReservationControllerTest.java
+++ src/test/java/edu/miu/sa/reservation/controller/ReservationControllerTest.java
@@ -7,6 +7,7 @@ import edu.miu.sa.reservation.service.ReservationService;
 import edu.miu.sa.reservation.utils.TestUtils;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -53,5 +55,26 @@ class ReservationControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message", is("Saved reservation")));
     }
+
+    @Test
+    public void should_return_reservation() throws Exception {
+        UUID reservationId = UUID.randomUUID();
+        UUID propertyId = UUID.randomUUID();
+        Reservation reservation = new Reservation(reservationId, "phatnguyen@outlook.com", propertyId, "Credit", 200, 3, 600);
+        Mockito.when(reservationService.getById(reservationId)).thenReturn(reservation);
+        mockMvc.perform(get("/api/reservations/" + reservationId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(reservation.getId().toString())))
+                .andExpect(jsonPath("$.email", is(reservation.getEmail())))
+                .andExpect(jsonPath("$.propertyId", is(reservation.getPropertyId().toString())))
+                .andExpect(jsonPath("$.paymentType", is(reservation.getPaymentType())))
+                .andExpect(jsonPath("$.price", is(reservation.getPrice())))
+                .andExpect(jsonPath("$.night", is(reservation.getNight())))
+                .andExpect(jsonPath("$.total", is(reservation.getTotal())));
+
+
+    }
+
 
 }
